8289b5328ad31cfc711f217207145829d75f2ed2
Steven Eisinger <steven.a.eisinger@gmail.com>
2022-01-20 23:30:48 +0100

    Modify autorun file to use different values for PID weights

---
+++ train-pid.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Train PandaReach-v2 and PandaReachJoints-v2 with PPO and other algorithms.
+
+import subprocess
+from pathlib import Path
+import colorama
+from colorama import Fore, Style
+
+colorama.init()
+
+
+def run():
+    source_dir = Path(__file__).parent
+    envs = ["PandaReach-v2", "PandaReachJoints-v2"]
+    algos = ["ppo"]
+    P_values = (1.0, 2.0, 5.0)
+    I_values = (1.0, 2.0, 5.0)
+    D_values = (1.0, 2.0, 5.0)
+    reward_weights = ((p,i,d) for p in P_values for i in I_values for d in D_values)
+
+    for algo in algos:
+        for env in envs:
+            for weights in reward_weights:
+                print(Style.BRIGHT + Fore.MAGENTA + f"Training {env} with {algo}, weights: {weights}" + Style.RESET_ALL)
+                P,I,D = weights
+                subprocess.call(
+                    f"python3 {source_dir}/train.py --env {env} --algo {algo} --tensorboard-log {source_dir}/autoruns/pid/{P}_{I}_{D} --save-freq 100000 --env-kwargs reward_type:\"dense\" reward_weights:({P},{I},{D})".split()
+                )
+
+
+if __name__ == "__main__":
+    run()
