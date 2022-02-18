43c27c97354344dd73a1678eb45588bb8aa2e95b
Steven Eisinger <steven.a.eisinger@gmail.com>
2022-02-18 12:31:18 +0100

    Fixed traning script to change the obstacles

--- train-obstacles.py
+++ train-obstacles.py
@@ -21,7 +21,7 @@ def run():
                 for reward_type in reward_types:
                     print(Style.BRIGHT + Fore.MAGENTA + f"Training {env} with {algo}, obstacle: {obstacle}, reward: {reward_type}" + Style.RESET_ALL)
                     subprocess.call(
-                        f"python3 {source_dir}/train.py --env {env} --algo {algo} --tensorboard-log {source_dir}/autoruns/obstacles/{obstacle}/{reward_type} --save-freq 100000 --env-kwargs reward_type:{repr(reward_type)}".split()
+                        f"python3 {source_dir}/train.py --env {env} --algo {algo} --tensorboard-log {source_dir}/autoruns/obstacles/{obstacle}/{reward_type} --save-freq 100000 --env-kwargs reward_type:{repr(reward_type)} obstacle_type:{repr(obstacle)}".split()
                     )
 
 
