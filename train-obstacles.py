3c106936f3f7fc08f17d56952411bb59035c0564
Steven Eisinger <steven.a.eisinger@gmail.com>
2022-04-07 16:05:54 +0200

    Update train obstacles with training weights

--- train-obstacles.py
+++ train-obstacles.py
@@ -18,13 +18,14 @@ def run():
     obstacles = ["bin"]
     # reward_types = ["pid", "dense", "sparse"]
     reward_types = ["pid"]
+    P, I, D = [5.0, 5.0, 1.0]
     for algo in algos:
         for env in envs:
             for obstacle in obstacles:
                 for reward_type in reward_types:
                     print(Style.BRIGHT + Fore.MAGENTA + f"Training {env} with {algo}, obstacle: {obstacle}, reward: {reward_type}" + Style.RESET_ALL)
                     subprocess.call(
-                        f"python3 {source_dir}/train.py --env {env} --algo {algo} --tensorboard-log {source_dir}/autoruns/obstacles/{obstacle}/{reward_type} --save-freq 100000 --env-kwargs reward_type:{repr(reward_type)} obstacle_type:{repr(obstacle)}".split()
+                        f"python3 {source_dir}/train.py --env {env} --algo {algo} --tensorboard-log {source_dir}/autoruns/obstacles/{obstacle}/{reward_type} --save-freq 100000 --env-kwargs reward_type:{repr(reward_type)} obstacle_type:{repr(obstacle)} reward_weights:[{P},{I},{D}]".split()
                     )
 
 
